cb5f309cc90f0ac150507891623e057c9c7d5fed
jovyan <jovanprodanov123@gmail.com>
2024-01-10 23:27:46 +0100

    initialize project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module avtekgroup.avtek {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens avtekgroup.avtek to javafx.fxml;
+    exports avtekgroup.avtek;
+}
