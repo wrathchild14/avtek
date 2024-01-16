a1304c56547ff7d5ec40cf38c937b491b502dcf4
jovyan <jovanprodanov123@gmail.com>
2024-01-16 19:20:01 +0100

    disable button when not chosen

--- src/main/java/avtekgroup/avtek/CarChoiceController.java
+++ src/main/java/avtekgroup/avtek/CarChoiceController.java
@@ -14,6 +14,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class CarChoiceController {
+    public Button confirmButton;
     @FXML
     private Label selectedCarLabel;
 
@@ -33,6 +34,7 @@ public class CarChoiceController {
         Image image = loadImage(imagePath);
         carImageView.setImage(image);
         carImageView.setVisible(true);
+        confirmButton.setDisable(false);
     }
 
     private String getCarImagePath(String carModel) {
